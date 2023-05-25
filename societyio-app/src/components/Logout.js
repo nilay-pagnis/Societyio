3a47ecb23ad37fcbc63f006a8c4d4cd3e91037fe
= <=>
2023-05-26 01:20:48 +0530

    logout page additional text removed

--- societyio-app/src/components/Logout.js
+++ societyio-app/src/components/Logout.js
@@ -2,29 +2,29 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-    const navigate = useNavigate();
-    useEffect(() => {
-        fetch("/logout", {
-        method: "GET",
-        headers: {
-            Accepts: "application/json",
-            "Content-Type": "application/json",
-        },
-        credentials: "include",
-        })
-        .then((res) => {
-            navigate("/login");
-            if (res.status !== 200) {
-            const error = new Error(res.error);
-            throw error;
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    });
+  const navigate = useNavigate();
+  useEffect(() => {
+    fetch("/logout", {
+      method: "GET",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((res) => {
+        navigate("/login");
+        if (res.status !== 200) {
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-  return <>Logout Page</>;
+  return <></>;
 };
 
 export default Logout;
